1ee73625f4cd6838e14c8dd5625d65b0b5346a42
Lily Le <lmlt8180@gmail.com>
2024-12-02 23:01:34 -0500

    import hero component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,14 @@ import Header from "./components/Header/Header";
 
 // library
 import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero/Hero";
 
 export default function App() {
 
   return (
     <BrowserRouter>
         <Header />
+        <Hero />
     </BrowserRouter>
   )
 };
