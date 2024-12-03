d61a0caf49b0b6bbdbf2285ec3cbda85bbd03c22
Lily Le <lmlt8180@gmail.com>
2024-12-03 14:09:40 -0500

    update home page
    - add section for work (where carousel will be)
    - add section for community (to learn more about The Verse)
    - add section for support

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -9,6 +9,62 @@ export default function HomePage() {
                     <source src="/assets/videos/placeholder-video.mp4" />
                 </video>
             </article>
+            
+            <div className="home__work">
+                <h2 className="home__work-header">
+                    What We're Building
+                </h2>
+
+                {/* carousel here */}
+            </div>
+
+            <div className="home__community">
+                <div className="home__community-content">
+                    <p className="home__community-text home__community-text--bold">
+                        Make an Impact
+                    </p>
+
+                    <h2 className="home__community-header">
+                        Join Our Community
+                    </h2>
+
+                    <p className="home__community-text">
+                        The Verse is a global community of inspired gamers, developers, designers,
+                        scientists, artists, visionaries, teachers, and students who join together
+                        to passionately contribute their wide-ranging expertise to invent experiences
+                        that enhance human flourishing.
+                    </p>
+
+                    <p className="home__community-button">
+                        Learn More
+                    </p>
+                </div>
+
+                <div className="home__community-image-container">
+                    <img 
+                        className="home__community-image"
+                        src="/assets/images/placeholder-image.jpg"
+                        alt="The Verse team members in a video meeting"
+                    />
+                </div>
+            </div>
+
+            <div className="home__support">
+                <div className="home__support-container">
+                    <h2 className="home__support-heading">
+                        Support The Verse
+                    </h2>
+
+                    <p className="home__support-text">
+                        Your contribution go directly towards a team of 50 people who donate ~20hrs
+                        per week to build games and experiences that impact health and consciousness.
+                    </p>
+
+                    <p className="home__support-button">
+                        Donate
+                    </p>
+                </div>
+            </div>
         </section>
     )
 };
