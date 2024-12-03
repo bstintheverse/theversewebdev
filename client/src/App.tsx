0a9e262d683301072f1996696b2df1f3815a8e95
Lily Le <lmlt8180@gmail.com>
2024-12-03 13:19:36 -0500

    import home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,12 @@ import "./styles/global.scss";
 
 // components
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+import HomePage from "./pages/Home/Home";
 
 // library
 import { BrowserRouter } from "react-router-dom";
-import Hero from "./components/Hero/Hero";
-import Footer from "./components/Footer/Footer";
 
 export default function App() {
 
@@ -15,6 +16,9 @@ export default function App() {
     <BrowserRouter>
         <Header />
         <Hero />
+        <main>
+          <HomePage />
+        </main>
         <Footer />
     </BrowserRouter>
   )
