2bc0fa7def0c535822b1c79bb47ca7045112235e
Lily Le <lmlt8180@gmail.com>
2024-12-02 23:37:43 -0500

    create footer component and stylesheet
    - add skeleton code

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+// styling
+import "./Footer.scss";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            Footer placeholder
+        </footer>
+    )
+};
