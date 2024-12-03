16f6a19d1a124d036b01971eb3bc1c6e844019bd
Lily Le <lmlt8180@gmail.com>
2024-12-02 23:27:29 -0500

    create hero component
    - added hero video
    - added hero subheader

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,7 +1,22 @@
+// styling
+import "./Hero.scss";
+
 export default function Hero() {
     return (
         <section className="hero">
-            Hero placeholder
+            <video
+                playsInline
+                autoPlay
+                muted
+                loop
+                className="hero__video"
+            >
+                <source src="/assets/videos/hero.mp4" type="video/mp4" />
+            </video>
+            <h1 className="hero__header">
+                What if games and digital experiences could make you feel 
+                happier, healthier, and more hopeful?
+            </h1>
         </section>
     )
 };
