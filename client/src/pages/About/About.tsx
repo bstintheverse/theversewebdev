d2a07297dcaacde48e66383e5fce42ab8f347901
Lily Le <lmlt8180@gmail.com>
2024-12-03 20:54:24 -0500

    create about us page and add boiler plate code

---
+++ client/src/pages/About/About.tsx
@@ -0,0 +1,14 @@
+//styling
+import "./About.scss";
+
+export default function AboutPage() {
+    return (
+        <section className="about">
+            <p>
+                About Placeholder
+            </p>
+
+
+        </section>
+    )
+};
