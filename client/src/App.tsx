442a1dc2dfaed1528ec79964b078510d713d9655
Lily Le <lmlt8180@gmail.com>
2024-12-03 21:59:01 -0500

    import donate and not found url path
    - create wildcard path and redirect to not found page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,11 +8,13 @@ import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/Home/Home";
 
 // library
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import AboutPage from "./pages/About/About";
 import ExperiencesPage from "./pages/Experiences/Experiences";
 import OurPeoplePage from "./pages/OutPeople/OurPeople";
 import ContactPage from "./pages/Contact/Contact";
+import DonatePage from "./pages/Donate/Donate";
+import NotFoundPage from "./pages/NotFound/NotFound";
 
 export default function App() {
 
@@ -27,6 +29,9 @@ export default function App() {
           <Route path="/experiences" element={ <ExperiencesPage /> } />
           <Route path="/our-people" element={ <OurPeoplePage /> } />
           <Route path="/contact-us" element={ <ContactPage /> } />
+          <Route path="/donate" element={ <DonatePage /> } />
+          <Route path="*" element={ <Navigate to="/404" /> } />
+          <Route path="/404" element={ <NotFoundPage /> } />
         </Route>
         </Routes>
         <Footer />
