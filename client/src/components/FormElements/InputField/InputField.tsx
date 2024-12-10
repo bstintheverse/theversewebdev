95a44c1fc86a889014374eec8aaf1c0570b0db68
Lily Le <lmlt8180@gmail.com>
2024-12-10 17:48:35 -0500

    update props

--- client/src/components/FormElements/InputField/InputField.tsx
+++ client/src/components/FormElements/InputField/InputField.tsx
@@ -4,16 +4,19 @@ import "./InputField.scss";
 
 interface InputFieldProps {
     className: string;
+    type: "text" | "email";
     name: string;
     value: string;
     placeholder: string;
     onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+    error: string;
 };
 
-export default function InputField({ className, name, value, placeholder, onChange}: InputFieldProps): JSX.Element {
+export default function InputField({ className, type = "text", name, value, placeholder, onChange, error}: InputFieldProps): JSX.Element {
     return (
         <input
             className={`input-field ${className}`}
+            type={type}
             name={name}
             value={value}
             placeholder={placeholder}
