dfc53f0c9907edab9b73941d364013a93a6bc2a7
Lily Le <lmlt8180@gmail.com>
2024-12-10 14:34:05 -0500

    update donate page
    - update content

--- client/src/pages/Donate/Donate.tsx
+++ client/src/pages/Donate/Donate.tsx
@@ -4,9 +4,24 @@ import "./Donate.scss";
 export default function DonatePage() {
     return (
         <section className="donate">
-            <p>
-                Donate Placeholder
-            </p>
+            <div className="donate__container">
+                <h2 className="donate__subheader">
+                    Coming Soon
+                </h2>
+
+                <p className="donate__text--white">
+                    Our donation page is launching soon! We're working hard to make it easy for you to 
+                    support our cause. Thank you for your interest in supporting our mission!
+                </p>
+            </div>
+
+            <div className="donate__content">
+                <img 
+                    className="donate__wip-gif"
+                    src="/assets/images/wip.gif"
+                    alt="Animated textbox with WIP text"
+                />
+            </div>
         </section>
     )
 };
