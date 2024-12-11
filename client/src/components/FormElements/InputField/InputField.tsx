2d075add2175d9422e6e7eb2c21575773c9705bd
Lily Le <lmlt8180@gmail.com>
2024-12-11 14:23:15 -0500

    add conditional operator to type prop and delete error prop

--- client/src/components/FormElements/InputField/InputField.tsx
+++ client/src/components/FormElements/InputField/InputField.tsx
@@ -4,15 +4,14 @@ import "./InputField.scss";
 
 interface InputFieldProps {
     className: string;
-    type: "text" | "email";
+    type?: "text" | "email";
     name: string;
     value: string;
     placeholder: string;
     onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-    error: string;
 };
 
-export default function InputField({ className, type = "text", name, value, placeholder, onChange, error}: InputFieldProps): JSX.Element {
+export default function InputField({ className, type = "text", name, value, placeholder, onChange}: InputFieldProps): JSX.Element {
     return (
         <input
             className={`input-field ${className}`}
