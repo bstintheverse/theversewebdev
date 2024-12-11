a8a959190387f62b72d284229c98fa8a8dee0b34
Lily Le <lmlt8180@gmail.com>
2024-12-11 10:12:19 -0500

    create button component and update styling

---
+++ client/src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import "./Button.scss";
+
+interface ButtonProps {
+    className: string;
+    type?: "button" | "submit" | "reset";
+    children: string;
+};
+
+export default function Button({ className, type = "button", children }: ButtonProps): JSX.Element {
+    return (
+        <button className={className} type={type}>
+            {children}
+        </button>
+    )
+};
