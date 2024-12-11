4450d99a7139aed5f96819cb2fb9060a5c6e01ba
Lily Le <lmlt8180@gmail.com>
2024-12-11 11:06:32 -0500

    delete unused code and import game card component

--- client/src/pages/Experiences/Experiences.tsx
+++ client/src/pages/Experiences/Experiences.tsx
@@ -1,51 +1,31 @@
-//styling
+// styling
 import "./Experiences.scss";
 
+// data
 import { games } from "../../data/games";
-import { Link } from "react-router-dom";
+
+// component
+import GameCard from "../../components/Cards/GameCard/GameCard";
 
 export default function ExperiencesPage() {
     return (
         <section className="experiences">
             <article className="experience__cards">
                 <ul className="experience__card-list">
-                    {games.map((game) => {
-                        return (
-                            <li className="experience__card-item">
-                                <div className="experience__container">
-                                <h2 className="experience__header">
-                                    {game.name}
-                                </h2>
-                                    {game.video ? (
-                                        <video className="experience__video" controls>
-                                            <source src={game.video} />
-                                        </video>
-                                    ) : game.image ? (
-                                        <img 
-                                            className="experience__image"
-                                            src={game.image}
-                                            alt={game.alt}
-                                        />
-                                    ) : null}
-
-                                    <p className="experience__description">
-                                        {game.description}
-                                    </p>
-                                </div>
-
-                                <div className="experience__wrapper">
-                                    <Link to={game.link} className="experience__link">
-                                        <p className="experience__button">
-                                            Learn More
-                                        </p>
-                                    </Link>
-                                </div>
-                            </li>
-                        )
-                    })}
-                    
+                    {games.map((game) => (
+                        <GameCard
+                            key={game.id}
+                            id={game.id}
+                            name={game.name}
+                            video={game.video}
+                            image={game.image}
+                            alt={game.alt}
+                            description={game.description}
+                            link={game.link}
+                        />
+                    ))}
                 </ul>
             </article>
         </section>
-    )
+    );
 };
