2091f1be939a90b3ecf7ca675c7f7a44ceb38b60
Lily Le <lmlt8180@gmail.com>
2024-12-17 17:20:55 -0500

    update class names and content on terms page

--- client/src/pages/Terms/Terms.tsx
+++ client/src/pages/Terms/Terms.tsx
@@ -15,35 +15,33 @@ export default function TermsPage() {
                 </title>
             </Helmet>
 
-            <section className="privacy">
-                <h2 className="privacy__subheader">
+            <section className="terms">
+                <h2 className="terms__subheader">
                     Terms of Service
                 </h2>
 
-                <p className="privacy__last-update">
+                <p className="terms__last-update">
                     Last Updated: December 17, 2024
                 </p>
 
-                <p className="privacy__text">
-                    Welcome to the <span className="privacy__text--bold">The Verse</span>! Your
-                    privacy is important to us. This Privacy Policy outlines how we collect, use,
-                    and protect your personal information when you visit
-                    <a href="https://www.versebuilding.com">www.versebuilding.com</a>.
+                <p className="terms__text">
+                    Welcome to the <span className="terms__text--bold">The Verse</span> at <a className="terms__link" href="https://www.versebuilding.com">www.versebuilding.com</a>. 
+                    By accessing or using our website, you agree to comply with the following Terms of Service.
                 </p>
 
                 {termsOfService.map((term) => (
-                    <article className="privacy__policies">
-                        <h3 className="privacy__subheader--small">
+                    <article className="terms__policies">
+                        <h3 className="terms__subheader--small">
                             {term.title}
                         </h3>
 
-                        <p className="privacy__text">
+                        <p className="terms__text">
                             {term.content?.intro}
                         </p>
 
-                        <ul className="privacy__policy-list">
+                        <ul className="terms__policy-list">
                             {term.content?.points?.map((point) => (
-                                <li className="privacy__policy privacy__text">
+                                <li className="terms__policy terms__text">
                                     {point}
                                 </li>
                             ))}
@@ -51,11 +49,11 @@ export default function TermsPage() {
                     </article>
                 ))}
 
-                <h2 className="privacy__subheader">
-                    Contact Us
+                <h2 className="terms__subheader">
+                    Questions?
                 </h2>
 
-                <p className="privacy__text">
+                <p className="terms__text">
                     If you have any questions about these Terms, please contact us at <a className="terms__link" href="mailto:team@versebuilding.com">team@versebuilding.com</a>.
                 </p>
             </section>
