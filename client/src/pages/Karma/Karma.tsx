fb4d44e18d9efef4ae71017a57820b93c11d3da7
Lily Le <lmlt8180@gmail.com>
2025-01-28 10:56:55 -0500

    create boilerplate for karma page and stylesheet

---
+++ client/src/pages/Karma/Karma.tsx
@@ -0,0 +1,10 @@
+// styling
+import "./Karma.scss";
+
+export default function KarmaPage() {
+    return (
+        <section className="karma">
+            Karma placeholder
+        </section>
+    );
+};
