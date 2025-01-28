5eceb1a90b2e1a3a25bea42a35f00f8cab4203b7
Lily Le <lmlt8180@gmail.com>
2025-01-27 21:58:14 -0500

    add meta tag for mobile viewport

---
+++ client/src/data/carouselCards.js
@@ -0,0 +1,29 @@
+;
+const imagePath = "/assets/images/cards/carousel-cards";
+export const carouselCards = [
+    {
+        id: 1,
+        image: `${imagePath}/carousel-card-karma.png`,
+        alt: "Karma"
+    },
+    {
+        id: 2,
+        image: `${imagePath}/carousel-card-aib.png`,
+        alt: "Adventures in Breath"
+    },
+    {
+        id: 3,
+        image: `${imagePath}/carousel-card-walkxr.png`,
+        alt: "WalkXR"
+    },
+    {
+        id: 4,
+        image: `${imagePath}/carousel-card-testimonial1.png`,
+        alt: "Testimonial 1"
+    },
+    {
+        id: 5,
+        image: `${imagePath}/carousel-card-testimonial2.png`,
+        alt: "Testimonial 2"
+    }
+];
