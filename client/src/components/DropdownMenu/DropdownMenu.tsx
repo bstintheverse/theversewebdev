25133a94ac77f06a66b26c959ebcf478daae0177
Lily Le <lmlt8180@gmail.com>
2025-01-29 11:34:15 -0500

    update dropdown menu component
    - add state management for toggling on and closing menu bar

--- client/src/components/DropdownMenu/DropdownMenu.tsx
+++ client/src/components/DropdownMenu/DropdownMenu.tsx
@@ -4,38 +4,49 @@ import "./DropdownMenu.scss";
 
 // component
 import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from "react";
 
 export default function DropdownMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <div className="dropdown">
-            <button className="dropdown__button">
+            <button className="dropdown__button" onClick={toggleMenu}>
                 <MenuIcon
                     className="dropdown__menu-icon"
                 />
             </button>
 
-            <div className="dropdown__content">
+            <div className={`dropdown__content ${isOpen ? "open" : ""}`}>
                 <ul className="dropdown__list">
                     <li className="dropdown__item">
-                        <Link to="/about-us" className="dropdown__link">
+                        <Link to="/about-us" className="dropdown__link" onClick={closeMenu}>
                             About Us
                         </Link>
                     </li>
 
                     <li className="dropdown__item">
-                        <Link to="/experiences" className="dropdown__link">
+                        <Link to="/experiences" className="dropdown__link" onClick={closeMenu}>
                             Games & Digital Experiences
                         </Link>
                     </li>
 
                     <li className="dropdown__item">
-                        <Link to="/our-people" className="dropdown__link">
+                        <Link to="/our-people" className="dropdown__link" onClick={closeMenu}>
                             Our People
                         </Link>
                     </li>
 
                     <li className="dropdown__item">
-                        <Link to="/contact-us" className="dropdown__link">
+                        <Link to="/contact-us" className="dropdown__link" onClick={closeMenu}>
                             Contact Us
                         </Link>
                     </li>
