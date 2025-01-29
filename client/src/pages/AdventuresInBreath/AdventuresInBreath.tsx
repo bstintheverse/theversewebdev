355bab0c3ba462ca69bc7690a2628925b2935f3a
Lily Le <lmlt8180@gmail.com>
2025-01-29 10:35:08 -0500

    update aib page

--- client/src/pages/AdventuresInBreath/AdventuresInBreath.tsx
+++ client/src/pages/AdventuresInBreath/AdventuresInBreath.tsx
@@ -1,10 +1,36 @@
 // styling
 import "./AdventuresInBreath.scss";
 
+// data
+import { AdventuresInBreathInfo } from "../../data/adventuresInBreath";
+
 export default function AdventuresInBreathPage() {
     return (
         <section className="aib">
-            AIB Placeholder
+            {AdventuresInBreathInfo.headers.map((header) => (
+                <div 
+                    className="aib__container"
+                    key={header.id}
+                >
+                    <h2 className="aib__header">
+                        {header.title}
+                    </h2>
+
+                    <p className="aib__text">
+                        {header.content}
+                    </p>
+                    
+                    <div className={`aib__assets-container--${header.id}`}>
+                        {header.assets?.map((asset) => (
+                            <img
+                                className={`aib__image--${asset.id}`}
+                                src={asset.src}
+                                alt={asset.alt}
+                            />
+                        ))}
+                    </div>
+                </div>
+            ))}
         </section>
     );
 };
