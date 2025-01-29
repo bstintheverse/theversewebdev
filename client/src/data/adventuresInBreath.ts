a2588ae70190d5ea61e765e3e0482dfdd240da92
Lily Le <lmlt8180@gmail.com>
2025-01-29 10:34:56 -0500

    update placeholder image path

--- client/src/data/adventuresInBreath.ts
+++ client/src/data/adventuresInBreath.ts
@@ -25,5 +25,29 @@ interface AdventuresInBreathInfoProp {
 };
 
 export const AdventuresInBreathInfo: AdventuresInBreathInfoProp = {
-
+    headers: [
+        {
+            id: 1,
+            title: "Turn Every Breath Into a Journey",
+            content: "At Adventures in Breath, we gamify breathing practices to enhance health and wellbeing. Our mission is to make mindfulness fun and accessible for everyone. By combining engaging gameplay with scientifically-backed techniques, we help users improve their mental and physical health, reduce stress, and boost overall wellness. Venture on an adventure through the rich world of AiB, showcasing a diverse array of games, concepts, and imaginative ideas where breath serves as the core mechanic, whether it's navigating mystical landscapes or outmaneuvering adversaries.",
+            assets: [
+                {
+                    id: 1,
+                    type: "image",
+                    src: "/assets/images/placeholder-square.jpg",
+                    alt: "image placeholder"
+                }
+            ]
+        },
+        {   // add additional metadata once design is finalized
+            id: 2,
+            title: "Ready. Set. Breathe.",
+            content: "Our games are built on scientifically validated breath techniques, and use the player's breathing as the means of interaction within the game.  Through our games, digital experiences, and innovative concepts, we gamify therapeutic breath practices, creating engaging and immersive experiences that promote mindfulness and well-being."
+        },
+        {
+            id: 3,
+            title: "Play With Purpose, Breathe With Impact",
+            content: "Discover games where your breath guides the journey. Explore unique worlds, unlock mysteries, and experience the power of mindful interaction."
+        }
+    ]
 };
