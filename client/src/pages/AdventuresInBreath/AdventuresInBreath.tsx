5d0e470ed81159d1f38da4236ca1063d959d94b6
Lily Le <lmlt8180@gmail.com>
2025-01-29 09:56:11 -0500

    create aib boilerplate page and stylesheet

---
+++ client/src/pages/AdventuresInBreath/AdventuresInBreath.tsx
@@ -0,0 +1,10 @@
+// styling
+import "./AdventuresInBreath.scss";
+
+export default function AdventuresInBreathPage() {
+    return (
+        <section className="aib">
+            AIB Placeholder
+        </section>
+    );
+};
