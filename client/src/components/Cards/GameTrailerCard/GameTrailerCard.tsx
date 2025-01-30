a3604dfd924df1126aadeb45e0c5ea1df359ab2b
Lily Le <lmlt8180@gmail.com>
2025-01-30 13:16:33 -0500

    update game trailer card

--- client/src/components/Cards/GameTrailerCard/GameTrailerCard.tsx
+++ client/src/components/Cards/GameTrailerCard/GameTrailerCard.tsx
@@ -1,8 +1,43 @@
 // styling
 import "./GameTrailerCard.scss";
 
+
+// data
+import { AdventuresInBreathGames } from "../../../data/adventuresInBreath";
+
 export default function GameTrailerCard() {
     return (
-        
+        <article className="game-card__trailer">
+            <ul className="game-card__list">
+                {AdventuresInBreathGames.games.map((game) => (
+                    <li className="game-card__item" key={game.id}>
+                        <div className={`game-card__image-container game-card__image-container--${game.id}`}>
+                            {game.assets.slice(0, 2).map((asset, index) => (
+                                <img
+                                    key={index}
+                                    className="game-card__image"
+                                    src={asset.image}
+                                    alt={asset.alt}
+                                />
+                            ))}
+                        </div>
+
+                        <div className="game-card__info">
+                            <h2 className="game-card__header">
+                                {game.title}
+                            </h2>
+
+                            <p className="game-card__description">
+                                {game.content}
+                            </p>
+
+                            <p className="game-card__button">
+                                Watch Trailer
+                            </p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </article>
     );
 };
